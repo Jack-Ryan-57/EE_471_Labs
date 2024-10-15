8b6063a52d3a8ebd703b591356c22c154e0a30e4
Daniel Hoefer <dhoefer@calpoly.edu>
2024-10-15 00:34:18 -0700

    removed get_ik() from robot

--- classes/Robot.py
+++ classes/Robot.py
@@ -99,31 +99,6 @@ class Robot(OM_X_arm):
     def get_fk(self, joint_angles: np.array) -> np.array:
         return self.get_acc_mat(joint_angles)[3]
     
-    def get_ik(self, pose: np.array) -> np.array:
-        pose = [pose[0]/1000, pose[1]/1000, pose[2]/1000, np.deg2rad(pose[3])]
-        r = np.sqrt(pose[0]*pose[0] + pose[1]*pose[1])
-        r_w = r - self.L4*np.cos(pose[3])
-        z_w  = pose[2] - self.L1 - self.L4*np.sin(pose[3])
-        d_w = np.sqrt(r_w*r_w + z_w*z_w)
-        mu = np.atan2(z_w, r_w)
-        cos_beta = (self.L2*self.L2 + self.L3*self.L3 - d_w*d_w)/(2*self.L2*self.L3)
-        sin_beta = np.sqrt(1-cos_beta*cos_beta)
-        beta1 = np.atan2(sin_beta, cos_beta)
-        beta2 = np.atan2(-sin_beta, cos_beta)
-        cos_gamma = (d_w*d_w + self.L2*self.L2 - self.L3*self.L3)/(2*d_w*self.L2)
-        sin_gamma = np.sqrt(1-cos_gamma*cos_gamma)
-        gamma1 = np.atan2(sin_gamma, cos_gamma)
-        gamma2 = np.atan2(-sin_gamma, cos_gamma)
-        delta = np.atan2(self.l_22, self.l_21)
-        theta_2 = np.pi/2 - delta - gamma1 - mu
-        theta_3 = np.pi/2 + delta - beta1
-        elbow_up = [np.atan2(pose[1], pose[0]), theta_2, theta_3, -pose[3] - theta_2 - theta_3]
-        theta_2 = np.pi/2 - delta - gamma2 - mu
-        theta_3 = np.pi/2 + delta - beta2
-        elbow_down = [np.atan2(pose[1], pose[0]), theta_2, theta_3, -pose[3] - theta_2 - theta_3]
-        return np.rad2deg(elbow_up)
-        pass
-    
     '''
     Inputs: None
     Return: 4 × 4 numpy array representing the end-effector to base transformation for current joint angles
